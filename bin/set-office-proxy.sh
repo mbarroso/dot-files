778cea364c7f686d8815cccf32defbdf412b945d
Damian Noseda <damian.noseda@mercadolibre.com>
2011-12-21 11:26:15 -0200

    Update bin/set-office-proxy.sh

--- bin/set-office-proxy.sh
+++
@@ -1,3 +0,0 @@
-sudo sh -c 'echo Acquire::http::proxy \"http://ML\dnoseda:Ml123456@10.200.1.68:8080/\"";" >> /etc/apt/apt.conf';
-sudo sh -c 'echo Acquire::ftp::proxy \"ftp://ML\dnoseda:Ml123456@10.200.1.68:8080/\"";" >> /etc/apt/apt.conf';
-sudo sh -c 'echo Acquire::https::proxy \"https://ML\dnoseda:Ml123456@10.200.1.68:8080/\"";" >> /etc/apt/apt.conf'
