2d315e0ed9c10f0894926b58646589ae642da1fa
dnoseda <damian.noseda@mercadolibre.com>
2011-12-21 10:55:02 -0300

    fixed

--- rm.sh
+++ rm.sh
@@ -1,6 +1,6 @@
-git filter-branch --index-filter 'git rm --cached --ignore-unmatch bin/tweet.sh' --prune-empty -- --all
-git filter-branch --index-filter 'git rm --cached --ignore-unmatch bin/check-domain.sh' --prune-empty -- --all
-git filter-branch --index-filter 'git rm --cached --ignore-unmatch bin/cdslkey.ppk' --prune-empty -- --all
+git filter-branch --index-filter 'git rm --cached --ignore-unmatch bin/tweet.sh' --prune-empty -- --all -f
+git filter-branch --index-filter 'git rm --cached --ignore-unmatch bin/check-domain.sh' --prune-empty -- --all -f
+git filter-branch --index-filter 'git rm --cached --ignore-unmatch bin/cdslkey.ppk' --prune-empty -- --all -f
 echo bin/tweet.sh >> .gitignore
 echo bin/check-domain.sh >> .gitignore
 echo bin/cdslkey.ppk >> .gitignore
